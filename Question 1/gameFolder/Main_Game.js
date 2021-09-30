5ad1f52884bcd78e7aae5708e0607ae923483047
Andy Duong <johndoe@example.com>
2021-10-01 00:28:21 +1000

    PLS END ME

--- Question 1/gameFolder/Main_Game.js
+++ Question 1/gameFolder/Main_Game.js
@@ -1,64 +1,44 @@
-let radius = 10
-
-
+let Width = 1200;
+let Height = 700;
 function setup() {
-  createCanvas(1200, 600);
-  noCursor();
-  
-  //Creating Groups 
-  town = new Group();
-  missle = new Group();
-  turrent = new Group();
+  createCanvas(Width, Height)
 }
-
-function backgroundImage(){
-  image(bgImage, 0, 0);
-}
-
-
 
 function draw() {
-  backgroundImage();
-  buildings();
-  //audio();
-  shooting();
-  mouseAim();
-  title();
+starBackground()
+score()
+buildings()
+shooting()
+mouseAim()
+loading()
+
+}
+
+function starBackground(){
+  image(backgroundImg, 0,0)
 }
 
 function buildings() {
-  drawSprites(town)
-  //Buildings 1
-  building = createSprite(100, 540)
-  building.addImage(city)
-  city.resize(200, 130)
-  building.addToGroup(town)
+  buildingSprite = createSprite(0, 600);
+  buildingSprite.addImage(buildingImg)
+  
 
-  //Buildings 2
-  building2 = createSprite(350, 540)
-  building2.addImage(city)
-  building2.addToGroup(town)
 
-  //Buildings 3
-  building3 = createSprite(850, 540)
-  building3.addImage(city)
-  building3.addToGroup(town)
-
-  //Buildings 4
-  building4 = createSprite(1100, 540)
-  building4.addImage(city)
-  building4.addToGroup(town)
+ 
 } 
 function shooting() {
-drawSprites(turrent)
-drawSprites(missle)
-shooters = createSprite(600, 565)
-shooters.addImage(redTurrent)
-shooters.addToGroup(turrent)
+
 } 
 function mouseAim(){
-  noFill()
-  stroke(255)
-  strokeWeight(2)
-  ellipse(mouseX, mouseY, radius, radius)
-}
+ }
+
+ function keyTyped(){
+   if (load == true && keyCode === ENTER){
+     load = false
+     ingame = true
+   } 
+ }
+
+ function score(){
+   rect(0, 600, 1200, 100)
+ }
--- Question 1/gameFolder/slider.js
+++
@@ -1,7 +0,0 @@
-function sliderSpeed(){
-    sliderSpd = createSlider(1, 100, 20, 1);
-    sliderSpd.postion(700,700)
-    slider.style('width', '80px');
-
-}
-
